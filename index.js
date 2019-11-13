5f64de16f37ce1b1fefa4d3fad55d80a290d9490
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-11-13 16:31:30 -0700

    update 11-13-19

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const STORE = [
             "3) recycle, replace, and repair", 
             "4) recycle, repair, and reuse"
         ],
-        answer: 2,
+        answer: "2) reduce, reuse, and recycle"
     },
       //1
       {
@@ -20,7 +20,7 @@ const STORE = [
           "3) plastic",
           "4) drinking glasses"
         ],
-        answer: 4,
+        answer: "4) drinking glasses"
       },
       //2
       {
@@ -31,17 +31,18 @@ const STORE = [
           "3) five - seven times", 
           "4) once"
         ],
-        answer: 3,
+        answer: "3) five - seven times"
       },
       //3
       {
         question: "Question 4: Which of these are not renewable energy resources?", 
         options: [
-            "1) sunlight", 
-            "2) wind", 
-            "3) geothermal heat", 
-            "4) natural gas"],
-        answer: 4,
+          "1) sunlight", 
+          "2) wind", 
+          "3) geothermal heat", 
+          "4) natural gas"
+        ],
+        answer: "4) natural gas"
       },
       //4
       {
@@ -52,7 +53,7 @@ const STORE = [
           "3) methanol", 
           "4) Biogas"
         ],
-        answer: 1,
+        answer: "1) ethanol"
       },
       //5
       {
@@ -63,7 +64,7 @@ const STORE = [
           "3) 2.5%", 
           "4) 25.5%"
         ],
-        answer: 3,
+        answer: "3) 2.5%"
       },
       //6
       {
@@ -74,7 +75,7 @@ const STORE = [
           "3) 0.05%", 
           "4) 1%"
         ],
-        answer: 2,
+        answer: "2) 0.003%"
       },    
 ];
 
@@ -82,146 +83,48 @@ let totalNumberOfQuestion = STORE.length;
 let questionNumber = 0;
 let score = 0;
 
-//Click .start-button adds .hidden class to Start Section, and removes .hidden class of Question Section
 function startQuiz() {
-  $('.start-container').on('click', '.start', function (event) {
-        $('.start-container').hide();
-        $('.question-container').show();
-        $('.options-form').show();
-        //$('.response').hide();
-        $('.question-score-container').show();
-        $('.final-container').hide();
-        questionNumber = 0;
-    })
-  displayQuestion();
-  displayOptions();
-};
-
-function currentQuestion() {
-  $('.question-number').empty();
-  $('.question-number').append(
-      `<p class= 'number'>Question: ${questionNumber + 1} out of 7</p>`
-  );
-};
-
-function currentScore() {
-  $('.question-score').empty();
-  $('.question-score').append(
-      `<p>Score: ${score} out of 7</p>`
-  );
-  console.log('currentScore ran');
-  console.log(score);
-};
-
-
-//display question gets called in submitAnswer
-function displayQuestion() {
-  /*$('.show-question').text(STORE[questionNumber].question);
-    console.log('questionDisplay ran', questionNumber);
-    currentQuestion();
-    currentScore();*/
-  //$('.question-container').show()
-  $('.question-container').empty();
-  $('.question-container').append(
-    `<p class="show-question">${STORE[questionNumber].question}</p>`
-  );
-    console.log('displayQuestion ran', questionNumber);
-    currentQuestion();
-    currentScore();
-}
-
-//display options gets called in submitAnswer
-function displayOptions() {
-
-  //empty options container
-  $('.show-option').empty();
-  const allOptions = STORE[questionNumber].options.length;
-  //populate empty .options-container
-  for (let i = 0; i < allOptions; i++) {
-      $('.options-container').append(
-          `<input class='show-option' type="radio" name='answer' value='${i}'>${STORE[questionNumber].options[i]}<br />`
-      );
-  }
-  console.log('displayOption ran')
-}
-
-function nextQuestion() { //called by right/wrongAnswer function
-  $('.response').on('click', '.nextButton', function (event){
-    $('.submit-answer').show();
-    $('.response').hide();
+  $('.start').click(function () {
+    $('.container').empty();
+    console.log('startQuiz ran')
+    questionNumber = 0;
+    displayQuestion();
   });
-  displayQuestion();
-  displayOptions();
-  console.log('nextQuestion ran');
-}
+};
 
-//what shows when you get the answer right gets called in submitAnswer
-function rightAnswer() {
-  $('.options-container').hide();
-  $('.submit-answer').hide();
-  $('.response').append(
-    `<h4>Your answer is correct!</h4>
-    <button type="button" class="nextButton button">Next Question</button>`
-  );
-  nextQuestion();
-}
-function wrongAnswer() {
-  $('.options-container').hide();
-  $('.submit-answer').hide();
-  $('.response').append(
-    `<h4>That is the wrong answer. It is actually: </h4>
-    <p class="correct-answer">${STORE[questionNumber].answer}</p>
-    <button type="button" class="nextButton button">Next Question</button>`
-  );
-  nextQuestion();
-}
+function displayQuestion() {
+  const allOptions = STORE[questionNumber].options.length;
+  for (let i = 0; i < allOptions; i++) {
+    $('.container').append(
+    `<form class="question-form" action="" method="post">
+      <fieldset ">
+        <legend class="show-question">${STORE[questionNumber].question}</legend>
+        <input class="show-option" type="radio" name="answer" value=${STORE[questionNumber].options}>${STORE[questionNumber].options[i]}<br />
+        <input class="show-option" type="radio" name="answer" value=${STORE[questionNumber].options}>${STORE[questionNumber].options[i]}<br />
+        <input class="show-option" type="radio" name="answer" value=${STORE[questionNumber].options}>${STORE[questionNumber].options[i]}<br />
+        <input class="show-option" type="radio" name="answer" value=${STORE[questionNumber].options}>${STORE[questionNumber].options[i]}<br />
+      </fieldset>
+        <button type="submit" class="submit-answer">Submit Answer</button>
+    </form>
+    <section class="question-score-container hidden"> 
+      <div class="question-number">
+          <p class="number">Question: ${questionNumber + 1} out of 7</p>
+      </div>
+      <div class="question-score">
+          <p class="score">Score: ${score} out of 7</p>
+      </div>
+      <button type="submit" class="submit-answer">Submit Answer</button>
+    </section>`,
+    );
+  }     
+  console.log('questionDisplay ran', questionNumber);
+};
 
-
-
-function submitAnswer() {
-  $('.question-score-container').on('click', '.submit-answer', function (event) {
-    event.preventDefault();
-    $('.response').show();
-    let selected = $('input:checked');
-    let selectedOption = selected.val(); //get value of selected item an insert into 'answer' variable
-    console.log('the selected option is', selectedOption);
-    let correct = STORE[questionNumber].answer;
-    if (selectedOption === correct) {
-      rightAnswer(); 
-    } else {
-      wrongAnswer(); 
-    }
-  }); 
-  console.log('submitAnswer ran')
-}
-
-
-//Click .reset-button adds .hidden class to Question and Final Section, and removes .hidden class of Start Section
-function takeAgain() {
-  $('.reset-button').click(function () {
-      location.reload();
-      $('.start-container').show();
-      $('.final-container').hide();
-      $('.question-section').hide();
-      console.log('Last.takeAgain ran');
-      
-      //reset questionNumber and score to 0
-      questionNumber = 0;
-      console.log('questionNumber')
-      score = 0;
-      console.log('score')
-  })
-}
 
 //call all the functions
 function quizApp() {
-    startQuiz();
-    //takeAgain();
-    submitAnswer();
-    //displayQuestion();
-    //displayOptions();
-    currentQuestion();
-    currentScore();
+  startQuiz();
+  //displayQuestion()
 }
 
 $(quizApp);
