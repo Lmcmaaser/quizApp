3627dbaf513d78aa9ff285ca46d29e8a432b54a7
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-11-14 14:33:21 -0700

    11-141-2019 submitAnswer and background update

--- index.js
+++ index.js
@@ -1,87 +1,88 @@
 //question data stored in object STORE
 const STORE = [
-    //0
-    {
-        question: "Question 1: What are the three R's of sustainable waste management?",
-        options: [
-            "1) reduce, recycle, and renew", 
-            "2) reduce, reuse, and recycle", 
-            "3) recycle, replace, and repair", 
-            "4) recycle, repair, and reuse"
-        ],
-        answer: "2) reduce, reuse, and recycle"
-    },
-      //1
-      {
-        question: "Question 2: Which of these items is not recycleable?",
-        options: [
-          "1) paper",
-          "2) aluminum cans", 
-          "3) plastic",
-          "4) drinking glasses"
-        ],
-        answer: "4) drinking glasses"
-      },
-      //2
-      {
-        question: "Question 3: How many times can paper be recycled?",
-        options: [
-          "1) indefinitely", 
-          "2) two - three times", 
-          "3) five - seven times", 
-          "4) once"
-        ],
-        answer: "3) five - seven times"
-      },
-      //3
-      {
-        question: "Question 4: Which of these are not renewable energy resources?", 
-        options: [
-          "1) sunlight", 
-          "2) wind", 
-          "3) geothermal heat", 
-          "4) natural gas"
-        ],
-        answer: "4) natural gas"
-      },
-      //4
-      {
-        question: "Question 5: Worldwide, what is the most common biofuel?",
-        options: [
-          "1) ethanol", 
-          "2) biodiesel", 
-          "3) methanol", 
-          "4) Biogas"
-        ],
-        answer: "1) ethanol"
-      },
-      //5
-      {
-        question: "Question 6: How much of the Earth's water is freshwater?",
-        options: [
-          "1) 49.5%", 
-          "2) 5.75%", 
-          "3) 2.5%", 
-          "4) 25.5%"
-        ],
-        answer: "3) 2.5%"
-      },
-      //6
-      {
-        question: "Question 7: Of the total amount of water available globally, how much is fresh and unpolluted?",
-        options: [
-          "1) 1.75%", 
-          "2) 0.003%", 
-          "3) 0.05%", 
-          "4) 1%"
-        ],
-        answer: "2) 0.003%"
-      },    
+  //1
+  {
+    question: "Question 1: What are the three R's of sustainable waste management?",
+    options: [
+      "1) reduce, recycle, and renew", 
+      "2) reduce, reuse, and recycle", 
+      "3) recycle, replace, and repair", 
+      "4) recycle, repair, and reuse"
+    ],
+    answer: "2) reduce, reuse, and recycle"
+  },
+  //2
+  {
+    question: "Question 2: Which of these items is not recycleable?",
+    options: [
+      "1) paper",
+      "2) aluminum cans", 
+      "3) plastic",
+      "4) drinking glasses"
+    ],
+    answer: "4) drinking glasses"
+  },
+  //3
+  {
+    question: "Question 3: How many times can paper be recycled?",
+    options: [
+      "1) indefinitely", 
+      "2) two - three times", 
+      "3) five - seven times", 
+      "4) once"
+    ],
+    answer: "3) five - seven times"
+  },
+  //4
+  {
+    question: "Question 4: Which of these are not renewable energy resources?", 
+    options: [
+      "1) sunlight", 
+      "2) wind", 
+      "3) geothermal heat", 
+      "4) natural gas"
+    ],
+    answer: "4) natural gas"
+  },
+  //5
+  {
+    question: "Question 5: Worldwide, what is the most common biofuel?",
+    options: [
+      "1) ethanol", 
+      "2) biodiesel", 
+      "3) methanol", 
+      "4) Biogas"
+    ],
+    answer: "1) ethanol"
+  },
+  //6
+  {
+    question: "Question 6: How much of the Earth's water is freshwater?",
+    options: [
+      "1) 49.5%", 
+      "2) 5.75%", 
+      "3) 2.5%", 
+      "4) 25.5%"
+    ],
+    answer: "3) 2.5%"
+  },
+  //7
+  {
+    question: "Question 7: Of the total amount of water available globally, how much is fresh and unpolluted?",
+    options: [
+      "1) 1.75%", 
+      "2) 0.003%", 
+      "3) 0.05%", 
+      "4) 1%"
+    ],
+    answer: "2) 0.003%"
+  },    
 ];
 
-let totalNumberOfQuestion = STORE.length;
+//let totalNumberOfQuestion = STORE.length;
 let questionNumber = 0;
 let score = 0;
+
 
 function startQuiz() {
   $('.start').click(function () {
@@ -92,7 +93,29 @@ function startQuiz() {
   })
 };
 
-function questionOptions() {
+function displayQuestion() {
+  $('.container').append(
+    `<section class="question-score-container"> 
+      <div class="question-number-score">
+        <p class="number">Question: ${questionNumber + 1} out of 7</p>
+        <p class="score">Score: ${score} out of 7</p>
+      </div>
+    </section>
+    <form class="question-form" action="" method="post">`
+  );
+  $('.question-form').append(
+    `<fieldset>
+      <legend class="show-question">${STORE[questionNumber].question}</legend>
+    </fieldset>
+      <button type="submit" class="submit-answer">Submit Answer</button>
+    </form>`
+  ); 
+  displayOptions();   
+  console.log('questionDisplay ran', questionNumber);
+  submitAnswer();
+};
+
+function displayOptions() {
   let allOptions = STORE[questionNumber].options.length;
   for (let i = 0; i < allOptions; i++) {
     $('.show-question').append(
@@ -101,30 +124,8 @@ function questionOptions() {
   }
 };
 
-function displayQuestion() {
-  $('.container').append(
-    `<section class="question-score-container hidden"> 
-      <div class="question-number">
-        <p class="number">Question: ${questionNumber + 1} out of 7</p>
-      </div>
-      <div class="question-score">
-        <p class="score">Score: ${score} out of 7</p>
-      </div>
-    </section>
-    <form class="question-form" action="" method="post">
-    <fieldset>
-      <legend class="show-question">${STORE[questionNumber].question}</legend>
-    </fieldset>
-      <button type="submit" class="submit-answer">Submit Answer</button>
-    </form>`,
-  );  
-  questionOptions();   
-  console.log('questionDisplay ran', questionNumber);
-  submitAnswer();
-};
-
 function submitAnswer() {
-  $('.question-score-container').on('click', '.submit-answer', function (event) {
+  $('.question-form').on('click', '.submit-answer', function (event) {
     event.preventDefault();
     $('.container').empty();
     let selected = $('input:checked');
@@ -140,16 +141,43 @@ function submitAnswer() {
       $('.container').append(
         `<h4>That is the wrong answer. It is actually: </h4>
         <p class="correct-answer">${STORE[questionNumber].answer}</p>
-        <button type="button" class="nextButton button">Next Question</button>`)
+        <button type="button" class="next-button button">Next Question</button>`)
     }
   }); 
 };
+
+function nextQuestion() {
+  $('.container').on('click', '.next-button', function (event) {
+    $('.container').empty();
+    $('.container').append(
+      `<section class="question-score-container"> 
+        <div class="question-number-score">
+          <p class="number">Question: ${questionNumber + 1} out of 7</p>
+          <p class="score">Score: ${score} out of 7</p>
+        </div>
+      </section>
+      <form class="question-form" action="" method="post">`
+    );
+  $('.question-form').append(
+    `<fieldset>
+      <legend class="show-question">${STORE[questionNumber].question}</legend>
+    </fieldset>
+      <button type="submit" class="submit-answer">Submit Answer</button>
+    </form>`
+  ); 
+  displayOptions();   
+  console.log('nextQuestion ran', questionNumber);
+  submitAnswer();
+  });
+};
+
 
 //call all the functions
 function quizApp() {
   startQuiz();
   //displayQuestion()
   //submitAnswer();
+  nextQuestion();
 };
 
 $(quizApp);
